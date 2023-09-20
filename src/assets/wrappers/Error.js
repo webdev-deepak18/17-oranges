ce2bba856469b7c4b7f3f2ef65258d0cf61f0b8b
webdev-deepak18 <webdev.deepak18@gmail.com>
2023-09-20 21:50:07 +0530

    tried different approach for error page

--- src/assets/wrappers/Error.js
+++ src/assets/wrappers/Error.js
@@ -6,20 +6,38 @@ import styled from "styled-components";
 
 
 const Wrapper = styled.div`
+.container{
   width: 100vw;
   height: 100vh;
-  background: red;
-  background: linear-gradient(190deg, rgba(0, 0, 0, 1) 5%, rgba(0, 0, 0, 0) 66%),
-    url("/src/assets/images/sad-austronaut.webp");
-  background-size: cover;
-  background-position: center;
-  display: grid;
-  align-content: end;
-
-  justify-content: center;
+  max-width: 1920px;
+  margin: 0 auto;
+  background: blue;
   position: relative;
+  display: grid;
+  justify-content: center;
+  align-content: end;;
+}
+.bg-img{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.overlay{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+ background: linear-gradient(190deg, rgb(0, 0, 0) 5%, rgba(0, 0, 0, 0) 66%);
+ top: 0;
+ left: 0;
+ z-index: 100;
+}
 
   .content {
+    position: relative;;
     background: rgba(0, 0, 0, 0.8);
     width: 95%;
     max-width: 800px;
@@ -28,6 +46,7 @@ const Wrapper = styled.div`
     text-align: center;
     border-radius: 15px;
     color: var(--white);
+    z-index: 500;
   }
   h2 {
     font-size: 2rem;
@@ -42,6 +61,7 @@ const Wrapper = styled.div`
     top: 5%;
     right: 10%;
     width: 150px;
+    z-index: 1000;
   }
 
   @media (min-width: 992px) {
